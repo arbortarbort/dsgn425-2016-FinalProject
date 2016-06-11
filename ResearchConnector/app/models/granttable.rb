042efa54a00086d12da02e3a7bb1ac779ecdd305
arbortarbort <hmalm2017@users.noreply.github.com>
2016-06-10 23:53:58 -0500

    final

--- ResearchConnector/app/models/granttable.rb
+++ ResearchConnector/app/models/granttable.rb
@@ -2,7 +2,7 @@ class Granttable < ActiveRecord::Base
 
   validates :field, :presence => true
   # validates_presence_of :grant_amt, :message => "Grant Amount can not be empty"
-  validates_presence_of :grant_amt, numericality: { greater_than: 0 }
+  # validates_presence_of :grant_amt, numericality: { greater_than: 0 }
   validates :abstract, presence: true
   validates :title, length: { maximum: 1000,
     too_long: "%{count} characters is the maximum allowed" }
